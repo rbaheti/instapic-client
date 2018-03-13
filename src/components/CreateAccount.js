0748faf0d445454bd78b3a2232be0939258fa8ce
Rashmi B <baheti.rash123@gmail.com>
2018-03-12 22:29:43 -0700

    use heroku server address in CreateAccount.js

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -26,7 +26,7 @@ class CreateAccount extends Component {
   createUser(e) {
     e.preventDefault();
     const userToSave = {username: this.state.username, password: this.state.password};
-    axios.post('http://localhost:3030/newuser', userToSave)
+    axios.post('https://instapic-server-rashmi.herokuapp.com/newuser', userToSave)
       .then((data) => {
         localStorage.setItem('localStorage-username', this.state.username);
         this.setState({isError: false});
